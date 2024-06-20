d026fc31f238347f3f5be401fe2e2657c05d4c5b
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-06-19 20:39:55 -0400

    dark and light mode toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
 	import Article from '$lib/Article.svelte';
+	import Moon from '$lib/Moon.svelte';
 	import SpyglassLogo from '$lib/SpyglassLogo.svelte';
+	import Sun from '$lib/Sun.svelte';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
+	let isDarkMode = false;
 </script>
 
 <svelte:head>
 	<title>The Spyglass</title>
 </svelte:head>
-<div class="flex justify-center items-center w-screen h-screen overflow-clip text-neutral-800 dark:text-neutral-200 bg-neutral-100 dark:bg-neutral-900 cursor-pointer select-none">
-	<div>
+<div
+	class="flex justify-center items-center w-screen h-screen overflow-clip dark:dark-theme light-theme select-none transition-colors duration-500"
+	class:dark={isDarkMode}
+>
+	<button
+		class="absolute top-10 left-10 w-12 aspect-square rounded-full hover:dark-theme dark:hover:light-theme p-3 transition-colors"
+		on:click={() => (isDarkMode = !isDarkMode)}
+	>
+		{#if isDarkMode}
+			<Sun />
+		{:else}
+			<Moon />
+		{/if}
+	</button>
+
+	<div class="cursor-pointer">
 		<h2 class="font-serif italic text-2xl" style="line-height: 0.6">the</h2>
 		<h1 class="font-serif text-5xl">spyglass</h1>
-		<SpyglassLogo class="fill-neutral-800 dark:fill-neutral-200 mt-2 mx-auto w-[60%]" />
+		<SpyglassLogo class="mt-2 mx-auto w-[60%]" />
 	</div>
 	<div
 		class="bottom-0 right-0 absolute py-10 pr-10 font-serif flex flex-col gap-4 items-end max-h-screen overflow-y-auto"
@@ -25,6 +42,3 @@
 		{/each}
 	</div>
 </div>
-
-<style>
-</style>
