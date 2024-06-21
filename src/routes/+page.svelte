1a0f3e3c056e7060732495951be76a4aa4f3bba4
andrew.chu206 <andrew.chu206@gmail.com>
2024-06-21 23:11:13 +0000

    animation on dark mode toggle + spyglass tube

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import Article from '$lib/Article.svelte';
-	import Moon from '$lib/Moon.svelte';
 	import SpyglassLogo from '$lib/SpyglassLogo.svelte';
-	import Sun from '$lib/Sun.svelte';
 	import type { PageServerData } from './$types';
+	import Icon from '@iconify/svelte';
 
 	export let data: PageServerData;
 	let isDarkMode = false;
+	let isHoveringDarkModeToggle = false;
 </script>
 
 <svelte:head>
@@ -17,20 +17,24 @@
 	class:dark={isDarkMode}
 >
 	<button
-		class="absolute top-10 left-10 w-12 aspect-square rounded-full hover:dark-theme dark:hover:light-theme p-3 transition-colors"
+		class="absolute top-10 left-10 w-9 aspect-square rounded-full p-1"
 		on:click={() => (isDarkMode = !isDarkMode)}
+		on:mouseenter={() => (isHoveringDarkModeToggle = true)}
+		on:mouseleave={() => (isHoveringDarkModeToggle = false)}
 	>
 		{#if isDarkMode}
-			<Sun />
+			<Icon icon={isHoveringDarkModeToggle ? 'ph:sun-fill' : 'ph:sun'} class="w-full h-full" />
 		{:else}
-			<Moon />
+			<Icon icon={isHoveringDarkModeToggle ? 'ph:moon-fill' : 'ph:moon'} class="w-full h-full" />
 		{/if}
 	</button>
 
 	<div class="cursor-pointer">
 		<h2 class="font-serif italic text-2xl" style="line-height: 0.6">the</h2>
 		<h1 class="font-serif text-5xl">spyglass</h1>
-		<SpyglassLogo class="mt-2 mx-auto w-[60%]" />
+		<div class="boop">
+			<SpyglassLogo class="mt-2 mx-auto w-[60%]" />
+		</div>
 	</div>
 	<div
 		class="bottom-0 right-0 absolute py-10 pr-10 font-serif flex flex-col gap-4 items-end max-h-screen overflow-y-auto"
@@ -42,3 +46,36 @@
 		{/each}
 	</div>
 </div>
+
+<style>
+	@keyframes boop {
+		0% {
+			transform: rotate(0deg);
+		}
+		15% {
+			transform: rotate(-4deg);
+		}
+		30% {
+			transform: rotate(4deg);
+		}
+		45% {
+			transform: rotate(-4deg);
+		}
+		60% {
+			transform: rotate(3deg);
+		}
+		75% {
+			transform: rotate(-3deg);
+		}
+		90% {
+			transform: rotate(1deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+
+	.boop:hover {
+		animation: 1s boop forwards running;
+	}
+</style>
