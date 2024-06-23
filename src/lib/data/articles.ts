ac17b200d3e43ad2bfe09f8706880f5c2ed10f0c
andrew.chu206 <andrew.chu206@gmail.com>
2024-06-23 20:45:41 +0000

    deleted extra cover images

--- src/lib/data/articles.ts
+++ src/lib/data/articles.ts
@@ -5,7 +5,7 @@ const ARTICLES: Article[] = [
         title: "summer issue twenty nineteen",
         shortTitle: "summer 2019",
         articleUrl: "/issues/summer_2019.pdf",
-        imgUrl: "/images/2019-summer/01.jpg",
+        imgUrl: "/images/2019-summer.jpg",
         date: "2019-11-06",
         archived: true
     },
@@ -13,7 +13,7 @@ const ARTICLES: Article[] = [
         title: "fall issue twenty nineteen",
         shortTitle: "fall 2019",
         articleUrl: "/issues/fall_2019.pdf",
-        imgUrl: "/images/2019-fall/01.jpg",
+        imgUrl: "/images/2019-fall.jpg",
         date: "2019-12-10",
         archived: true
     },
@@ -21,7 +21,7 @@ const ARTICLES: Article[] = [
         title: "winter issue twenty twenty",
         shortTitle: "winter 2020",
         articleUrl: "/issues/winter_2020.pdf",
-        imgUrl: "/images/2020-winter/01.jpg",
+        imgUrl: "/images/2020-winter.jpg",
         date: "2020-03-02",
         archived: true
     },
@@ -29,7 +29,7 @@ const ARTICLES: Article[] = [
         title: "spring issue twenty twenty",
         shortTitle: "spring 2020",
         articleUrl: "/issues/spring_2020.pdf",
-        imgUrl: "/images/2020-spring/2.jpg",
+        imgUrl: "/images/2020-spring.jpg",
         date: "2020-05-03",
         archived: true
     },
@@ -37,7 +37,7 @@ const ARTICLES: Article[] = [
         title: "summer issue twenty twenty",
         shortTitle: "summer 2020",
         articleUrl: "/issues/summer_2020.pdf",
-        imgUrl: "/images/2020-summer/1.jpg",
+        imgUrl: "/images/2020-summer.jpg",
         date: "2020-06-15",
         archived: true
     },
@@ -45,7 +45,7 @@ const ARTICLES: Article[] = [
         title: "fall issue twenty twenty",
         shortTitle: "fall 2020",
         articleUrl: "/issues/fall_2020.pdf",
-        imgUrl: "/images/2020-fall/lake.jpg",
+        imgUrl: "/images/2020-fall.jpg",
         date: "2020-11-29",
         archived: true
     },
@@ -53,28 +53,127 @@ const ARTICLES: Article[] = [
         title: "winter issue twenty twenty-one",
         shortTitle: "winter 2021",
         articleUrl: "https://issuu.com/rhhsspyglass/docs/winter_2021_compressed",
-        imgUrl: "/images/2021-winter/02.jpg",
+        imgUrl: "/images/2021-winter.jpg",
         date: "2021-02-08",
+        archived: true
     },
     {
         title: "spring issue twenty twenty-one",
         shortTitle: "spring 2021",
         articleUrl: "https://issuu.com/rhhsspyglass/docs/spring_2021_compressed_modified",
-        imgUrl: "/images/2021-spring/01.jpg",
+        imgUrl: "/images/2021-spring.jpg",
         date: "2021-04-30",
+        archived: true
     },
     {
         title: "summer issue twenty twenty-one",
         shortTitle: "summer 2021",
         articleUrl: "https://issuu.com/rhhspyglass2021/docs/summer_issue_2021_2",
-        imgUrl: "/images/2021-summer/01.jpg",
-        date: "2021-06-10",
+        imgUrl: "/images/2021-summer.jpg",
+        date: "2021-06-21",
+        archived: true
+    },
+    {
+        title: "fall issue twenty twenty-one",
+        shortTitle: "fall 2021",
+        articleUrl: "",
+        imgUrl: "/images/2021-fall.jpg",
+        date: "2021-11-22",
+        archived: true
+    },
+    {
+        title: "winter issue twenty twenty-two",
+        shortTitle: "winter 2022",
+        articleUrl: "",
+        imgUrl: "/images/2022-winter.jpg",
+        date: "2022-01-24",
+        archived: true
+    },
+    {
+        title: "spring issue twenty twenty-two",
+        shortTitle: "spring 2022",
+        articleUrl: "",
+        imgUrl: "/images/2022-spring.jpg",
+        date: "2022-03-28",
+        archived: true
+    },
+    {
+        title: "summer issue twenty twenty-two",
+        shortTitle: "summer 2022",
+        articleUrl: "",
+        imgUrl: "/images/2022-summer.jpg",
+        date: "2022-06-13",
+        archived: true
+    },
+    {
+        title: "fall issue twenty twenty-two",
+        shortTitle: "fall 2022",
+        articleUrl: "",
+        imgUrl: "/images/2022-fall.jpg",
+        date: "2022-11-10",
+        archived: true
+    },
+    {
+        title: "winter issue twenty twenty-three",
+        shortTitle: "winter 2023",
+        articleUrl: "",
+        imgUrl: "/images/2023-winter.jpg",
+        date: "2023-02-15",
+        archived: true
+    },
+    {
+        title: "spring issue twenty twenty-three",
+        shortTitle: "spring 2023",
+        articleUrl: "",
+        imgUrl: "/images/2023-spring.jpg",
+        date: "2023-04-17",
+        archived: true
+    },
+    {
+        title: "summer issue twenty twenty-three",
+        shortTitle: "summer 2023",
+        articleUrl: "",
+        imgUrl: "/images/2023-summer.jpg",
+        date: "2023-06-11",
+        archived: true
+    },
+    {
+        title: "director's cut twenty twenty-three",
+        shortTitle: "director's cut 2023",
+        articleUrl: "",
+        imgUrl: "/images/2023-directors-cut.jpg",
+        date: "2023-06-15",
+        archived: true
+    },
+    {
+        title: "fall issue twenty twenty-three",
+        shortTitle: "fall 2023",
+        articleUrl: "",
+        imgUrl: "/images/2023-fall.jpg",
+        date: "2023-11-15",
+        archived: false
+    },
+    {
+        title: "winter issue twenty twenty-four",
+        shortTitle: "winter 2024",
+        articleUrl: "",
+        imgUrl: "/images/2024-winter.jpg",
+        date: "2024-02-05",
+        archived: false
+    },
+    {
+        title: "spring issue twenty twenty-four",
+        shortTitle: "spring 2024",
+        articleUrl: "",
+        imgUrl: "/images/2024-spring.jpg",
+        date: "2024-04-10",
+        archived: false
     },
     {
         title: "summer issue twenty twenty-four",
         shortTitle: "summer 2024",
         articleUrl: "/issues/summer_2024.pdf",
-        imgUrl: "/images/2024-summer/2024-summer.JPG",
+        imgUrl: "/images/2024-summer.jpg",
         date: "2024-06-10",
         archived: false
     }
