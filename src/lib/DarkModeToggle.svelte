3cfcfdd0317260c753c33011beb5b64408baa701
Andrew <67875282+andrewchu16@users.noreply.github.com>
2024-06-28 07:43:37 -0400

    added dark mode toggle icon fallback

--- src/lib/DarkModeToggle.svelte
+++ src/lib/DarkModeToggle.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	import { isDarkMode } from './stores/themeStore';
-	import Icon from '@iconify/svelte';
-	let isHoveringDarkModeToggle = false;
+	import Icon, { iconExists } from '@iconify/svelte';
+	let isHoveringDarkMode = false;
+
+    let icon: string = "ph:moon";
+    isDarkMode.subscribe((value) => { 
+        icon = value ? "ph:sun" : "ph:moon";
+    });
+
+    $: loaded = iconExists(icon);
 </script>
 
 <button
 	class="absolute top-10 left-10 w-9 aspect-square rounded-full p-1"
 	on:click={isDarkMode.toggle}
-	on:mouseenter={() => (isHoveringDarkModeToggle = true)}
-	on:mouseleave={() => (isHoveringDarkModeToggle = false)}
+	on:mouseenter={() => (isHoveringDarkMode = true)}
+	on:mouseleave={() => (isHoveringDarkMode = false)}
 	aria-label="Toggle Dark Mode"
 >
-	{#if $isDarkMode}
-		<Icon icon={isHoveringDarkModeToggle ? 'ph:sun-fill' : 'ph:sun'} class="w-full h-full" />
-	{:else}
-		<Icon icon={isHoveringDarkModeToggle ? 'ph:moon-fill' : 'ph:moon'} class="w-full h-full" />
-	{/if}
+    {#if loaded}
+	    <Icon icon="{icon}{isHoveringDarkMode ? '-fill' : ''}" class="w-full h-full" />
+    {:else}
+        <div class="w-full h-full border-2 border-neutral-800 dark:border-neutral-200 rounded-full hover:bg-neutral-800 dark:hover:bg-neutral-200" />
+    {/if}
 </button>
