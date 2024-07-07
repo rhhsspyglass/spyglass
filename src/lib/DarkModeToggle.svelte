16367d334aef9483a06581fe59ecd3a0071e2b01
andrew.chu206 <andrew.chu206@gmail.com>
2024-07-07 23:42:22 +0000

    created fallback icon component

--- src/lib/DarkModeToggle.svelte
+++ src/lib/DarkModeToggle.svelte
@@ -1,47 +1,23 @@
 <script lang="ts">
 	import { isDarkMode } from './stores/themeStore';
-	import Icon, { iconExists, loadIcon } from '@iconify/svelte';
-	let isHoveringDarkMode = false;
-
-	let icon: string = 'ph:moon';
-	isDarkMode.subscribe((value) => {
-		icon = value ? 'ph:sun' : 'ph:moon';
-	});
-
-	let loaded = false;
-	let update = 0;
-	const icons = ['ph:sun', 'ph:moon', 'ph:sun-fill', 'ph:moon-fill'];
-
-	$: {
-		update;
-
-		loaded = true;
-		icons.forEach((value) => {
-			const curLoaded = iconExists(value);
-
-			if (!curLoaded) {
-				loadIcon(value).then(() => {
-					update++;
-				}).catch(() => {
-				});
-			}
-			loaded = loaded && curLoaded;
-		});
-	}
+	import FallbackIcon from './FallbackIcon.svelte';
+	let isHovering = false;
 </script>
 
 <button
 	class="absolute top-10 left-10 w-9 aspect-square rounded-full p-1"
 	on:click={isDarkMode.toggle}
-	on:mouseenter={() => (isHoveringDarkMode = true)}
-	on:mouseleave={() => (isHoveringDarkMode = false)}
+	on:mouseenter={() => (isHovering = true)}
+	on:mouseleave={() => (isHovering = false)}
 	aria-label="Toggle Dark Mode"
 >
-	{#if loaded}
-		<Icon icon="{icon}{isHoveringDarkMode ? '-fill' : ''}" class="w-full h-full" />
-	{:else}
+	<FallbackIcon
+		icon="{$isDarkMode ? 'ph:sun' : 'ph:moon'}{isHovering ? '-fill' : ''}"
+		class="w-full h-full"
+	>
 		<div
+			slot="fallback"
 			class="w-full h-full border-2 border-neutral-800 dark:border-neutral-200 rounded-full hover:bg-neutral-800 dark:hover:bg-neutral-200"
 		/>
-	{/if}
+	</FallbackIcon>
 </button>
---
+++ src/lib/FallbackIcon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Icon, { iconExists, loadIcon } from '@iconify/svelte';
+    
+    
+	export let icon: string;
+	export let loaded = false;
+	
+	let className: string = '';
+	export { className as class };
+    
+    let update = 0;
+
+	$: {
+		update;
+
+		loaded = iconExists(icon);
+		if (!loaded) {
+			loadIcon(icon)
+				.then(() => update++)
+				.catch(() => {});
+		}
+	}
+</script>
+
+{#if loaded}
+	<Icon {icon} class={className} />
+{:else}
+	<slot name="fallback" />
+{/if}
