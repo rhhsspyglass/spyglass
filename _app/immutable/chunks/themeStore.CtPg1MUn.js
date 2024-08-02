6b24fb21324e1bc67a05c05a8c897c522f65af66
Harry Xu <92757553+HarryXu497@users.noreply.github.com>
2024-08-02 00:33:35 -0400

    Updates

--- _app/immutable/chunks/themeStore.CtPg1MUn.js
+++
@@ -1,1 +0,0 @@
-import{w as n}from"./paths.Bh1lM6GB.js";function c(o,t){return localStorage.getItem(o)??t}function s(o,t){localStorage.setItem(o,t)}function i(){const{subscribe:o,set:t,update:r}=n(c("theme",!1)==="true");return{subscribe:o,toggle:()=>{r(e=>(t(!e),s("theme",String(!e)),!e))},set:e=>{t(e),s("theme",String(e))}}}const a=i();a.subscribe(console.log);export{a as i};
---
+++ _app/immutable/entry/start.CCEOeSya.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DGB3cC2Q.js";export{t as start};
