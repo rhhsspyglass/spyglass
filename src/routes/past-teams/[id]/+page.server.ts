e8705274b49a0cfee417d17325b2f9a5a571ce73
Harry Xu <harryxu497@gmail.com>
2025-02-04 10:06:55 -0500

    added entries generator for the past-teams/[id] route

--- src/routes/past-teams/[id]/+page.server.ts
+++ src/routes/past-teams/[id]/+page.server.ts
@@ -1,5 +1,6 @@
 import PAST_TEAMS from "$lib/data/teams";
 import type { PageServerLoad } from "./$types";
+import type { EntryGenerator } from './$types';
 
 export const load: PageServerLoad = (req) => {
     const id = req.params.id;
@@ -8,4 +9,8 @@ export const load: PageServerLoad = (req) => {
 		team: PAST_TEAMS[id],
         id,
 	};
+};
+
+export const entries: EntryGenerator = () => {
+    return Object.keys(PAST_TEAMS).map(teamId => ({ id: teamId }))
 };
