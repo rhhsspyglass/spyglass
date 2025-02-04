44f36e0fac35d4c777a05a2f3bf12758c7b6a906
Harry Xu <harryxu497@gmail.com>
2025-02-04 14:08:28 -0500

    updated past teams

--- src/lib/data/teams.ts
+++ src/lib/data/teams.ts
@@ -6,19 +6,19 @@ const TEAM_2023_2024: Team = {
     units: [
         {
             positionName: "Copy Editor",
-            memberNames: ["Kate Shahidi"],
+            memberNames: ["Yasmin Hadizad"],
         },
         {
             positionName: "Photography Editor",
-            memberNames: ["Jolly Yan"],
+            memberNames: ["Sam Abdi"],
         },
         {
             positionName: "Design Editor",
-            memberNames: ["Katrina Lam", "Lynn He"],
+            memberNames: ["Lynn He"],
         },
         {
             positionName: "Associate Editor",
-            memberNames: ["Erin Chen"],
+            memberNames: ["Rianna Zhu"],
         },
         {
             positionName: "Copywriter",
@@ -28,7 +28,7 @@ const TEAM_2023_2024: Team = {
                 "Anna Pan",
                 "Eowyn Wong-Au",
                 "Elaine Wang",
-                "Jilian Ouyang",
+                "Jillian Ouyang",
                 "Kate Shahidi",
                 "Max Skidelsky",
                 "Rianna Zhu",
@@ -38,10 +38,11 @@ const TEAM_2023_2024: Team = {
         {
             positionName: "Photographer",
             memberNames: [
+                "Anson Lai",
                 "Audrey Lai",
                 "Daniel Yang",
-                "Emma Fodor",
                 "Emily Meng",
+                "Emma Fodor",
                 "Iris You",
                 "Jack Kang",
                 "Jolly Yan",
@@ -73,8 +74,78 @@ const TEAM_2023_2024: Team = {
     ]
 };
 
+const TEAM_2022_2023: Team = {
+    editorsInChiefNames: "Rachel Liu and Samantha Lee",
+    units: [
+        {
+            positionName: "Copy Editor",
+            memberNames: ["Karen Zhou"],
+        },
+        {
+            positionName: "Photography Editor",
+            memberNames: ["Sam Abdi"],
+        },
+        {
+            positionName: "Design Editor",
+            memberNames: ["Ellie Liang", "Lynn He"],
+        },
+        {
+            positionName: "Associate Editor",
+            memberNames: ["Elaine Wang"],
+        },
+        {
+            positionName: "Copywriter",
+            memberNames: [
+                "Andrew Yang",
+                "Anna Pan",
+                "Elaine Wang",
+                "Jason Wu",
+                "Karen Zhou",
+                "Kate Shahidi",
+                "Rianna Zhu",
+                "Samantha Lee",
+                "Sophie Druta",
+                "Yasmin Hadizad"
+            ],
+        },
+        {
+            positionName: "Photographer",
+            memberNames: [
+                "Viktor Ostapchuk",
+                "Jolly Yan",
+                "Rachel Liu",
+                "Annie Chen",
+                "Anson Lai",
+                "Sam Abdi"
+            ],
+        },
+        {
+            positionName: "Designer",
+            memberNames: [
+                "Fiona Xu",
+                "Grace Fan",
+                "Lynn He",
+                "Hannah Truong",
+                "Jillian Feng",
+                "Melanie Ong",
+                "Elina Lai",
+                "Ellie Liang",
+                "Samantha Lee",
+                "Rachel Liu"
+            ],
+        },
+        {
+            positionName: "Webmaster",
+            memberNames: [
+                "Melanie Lei",
+            ],
+        }
+    ]
+};
+
 const PAST_TEAMS: Record<string, Team> = {
     "2023-2024": TEAM_2023_2024,
+    "2022-2023": TEAM_2022_2023,
 }
 
 export default PAST_TEAMS;
